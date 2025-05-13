63e657db8431b133b1ecedacff7b678e56256aa0
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-05-12 20:34:14 -0400

    new: no longer using mason, manually installing LSP's with lspconfig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,20 +1,44 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    dependencies =  {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {'williamboman/mason.nvim'},           -- Optional
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- lsp-config
+    -- For all the information you need to learn about LSP's, type
+    -- :help lspconfig-all
+    -- :help ins-completion
+    {
+        -- Helper plugin to support vim globals along with lua_ls
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},         -- Required
-        {'hrsh7th/cmp-nvim-lsp'},     -- Required
-        {'hrsh7th/cmp-path'},         -- Optional
-        {'saadparwaiz1/cmp_luasnip'}, -- Optional
-        {'hrsh7th/cmp-nvim-lua'},     -- Optional
+        -- Install LSP's here!
+        config = function()
+            local lspconfig = require("lspconfig")
+            vim.lsp.enable('lua_ls')  -- Lua
+            vim.lsp.enable('clangd')  -- C / C++
+            vim.lsp.enable('pyright') -- Python
+            vim.lsp.enable('bashls')  -- bash files
+        end,
+    },
 
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},             -- Required
-    }
+    {
+        -- nvim-cmp
+        "hrsh7th/nvim-cmp",
+        version = false, -- last release is way too old
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+        },
+    },
 }
