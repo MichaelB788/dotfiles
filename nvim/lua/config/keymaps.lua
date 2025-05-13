668eba06e97ab2b6d98c3c7c467401ada9fe4694
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-05-12 20:16:15 -0400

    diff: cleaned up plugin-specific keymaps, adding new lsp keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,31 +1,36 @@
 -- For help on key notation, type :h key-notation
 -- Globals
-vim.g.mapleader = " "                        -- Map leader
-
-local km = vim.keymap;
-local telescope = require('telescope.builtin')
-
--- Plugin-specific
-km.set('n', '<leader>ff', telescope.find_files, {})          -- Fuzzy find all files in cwd
-km.set('n', '<leader>fg', telescope.git_files, {})           -- Fuzzy find all git files
-km.set('n', '<leader>e', ':NvimTreeFindFileToggle<Enter>')   -- Toggle explorer
+vim.g.mapleader = " " -- Map leader
+local km = vim.keymap
 
 -- System clipboard
-km.set('n', 'yy', '"+Y')                     -- Yank line into system clipboard
-km.set('v', 'y', '"+y')                      -- Yank selected into system clipboard
-km.set('v', 'd', '"+d"')                     -- Cut selected into system clipboard
-km.set('n', 'p', '"+p')                      -- Paste from system clipboard below the cursor
-km.set('n', 'P', '"+P')                      -- Paste from system clipboard above the cursor
+km.set("n", "yy", '"+Y') -- Yank line into system clipboard
+km.set("v", "y", '"+y')  -- Yank selected into system clipboard
+km.set("v", "d", '"+d"') -- Cut selected into system clipboard
+km.set("n", "p", '"+p')  -- Paste from system clipboard below the cursor
+km.set("n", "P", '"+P')  -- Paste from system clipboard above the cursor
 
 -- File management
-km.set('n', '<leader>w', ':wall<Enter>')     -- Write to all files
-km.set('n', '<leader>q', ':q!<Enter>')       -- Force quit the current tab
+km.set("n", "<leader>w", "<cmd>wa!<CR>") -- Save all files
+km.set("n", "<leader>q", "<cmd>q!<CR>")  -- Force quit the current tab
 
 -- Window/Tab management
-km.set('n', '<leader>tn', ':tabnew<Enter>')  -- Open a new tab
-km.set('n', '<leader>sp', ':vsplit<Enter>')  -- Perform vertical split
+km.set("n", "<leader>tn", "<cmd>tabnew<CR>") -- Open a new tab
+km.set("n", "<leader>sp", "<cmd>vsplit<CR>") -- Perform vertical split
 
 -- Navigation
-km.set('n', 'L', 'gt')                       -- Move to left tab
-km.set('n', 'H', 'gT')                       -- Move to right tab
-km.set('n', 'w', '<C-w>')                    -- Pretense to window navigation
+km.set("n", "w", "<C-w>") -- Pretense to window navigation
+
+-- Code / LSP
+km.set('n', 'gD', vim.lsp.buf.declaration)                    -- Go to declaration
+km.set('n', 'gd', vim.lsp.buf.definition)                     -- Go to definition
+km.set('n', 'K', vim.lsp.buf.hover)                           -- Toggle Hover
+km.set('n', 'gi', vim.lsp.buf.implementation)                 -- Show implementation
+km.set('n', '<C-k>', vim.lsp.buf.signature_help)              -- Show signature
+km.set('n', '<leader>aw', vim.lsp.buf.add_workspace_folder)   -- Add a workspace folder
+km.set('n', '<space>rw', vim.lsp.buf.remove_workspace_folder) -- Remove a workspace folder
+km.set('n', '<space>D', vim.lsp.buf.type_definition)          -- Find the type definition 
+km.set('n', '<space>rn', vim.lsp.buf.rename)                  -- Rename a token
+km.set('n', '<space>ca', vim.lsp.buf.code_action)             -- List code actions
+km.set('n', 'gr', vim.lsp.buf.references)                     -- Go to references
+km.set('n', '<space>f', vim.lsp.buf.format)                   -- Format the file
