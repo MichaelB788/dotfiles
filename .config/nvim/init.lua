2eb41ed921d81656ed0762986f3d488efea0c843
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-06-25 21:00:59 -0400

    fixed: resourcing nvim config on startup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,45 +5,6 @@ vim.g.loaded_netrwPlugin = 1
 -- Lazy
 require("config.lazy")
 
--- Treesitter configs
-require'nvim-treesitter.configs'.setup{
-    ensure_installed = {
-        "c",
-        "c_sharp",
-        "cpp",
-        "bash",
-        "diff",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "printf",
-        "python",
-        "query",
-        "regex",
-        "rust",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-    },
-
-    auto_install = true,
-
-    highlight = { enable = true, },
-
-    indent = { enable = true, },
-}
-
 -- Lualine
 require("lualine").setup({})
 
