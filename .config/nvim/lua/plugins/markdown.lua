5e8c8c574d760314be8a1f7bdbc42589979f657d
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-08-30 19:48:20 -0400

    added neorg, fixed formatting

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,20 +1,31 @@
 return {
-    {
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {},
-        keys = {
-            {
-                "<leader>p", "<cmd>PasteImage<cr>",
-                desc = "Paste image from system clipboard"
-            },
-        },
-    },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you use the mini.nvim suite
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-    }
+	-- Paste images
+	{
+		"HakonHarnes/img-clip.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>p", "<cmd>PasteImage<cr>",
+				desc = "Paste image from system clipboard"
+			},
+		},
+	},
+
+	-- Neorg
+	{
+		"nvim-neorg/neorg",
+		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+		version = "*", -- Pin Neorg to the latest stable release
+		config = true,
+	},
+
+	-- Makes markdown files look nicer
+	{
+		'MeanderingProgrammer/render-markdown.nvim',
+		dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you use the mini.nvim suite
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	}
 }
--- .config/nvim/lua/plugins/visual.lua
+++ .config/nvim/lua/plugins/visual.lua
@@ -1,38 +1,53 @@
 return {
 	-- Treesitter
-  {
+	{
 		"nvim-treesitter/nvim-treesitter",
 		branch = 'master',
 		lazy = false,
 		build = ":TSUpdate"
 	},
 
-  -- catppuccin / colorscheme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+	-- catppuccin / colorscheme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
 
-    opts = {
-      lsp_trouble = true,
-      dashboard = true,
-      neotree = true,
-      native_lsp = {
-        enabled = true,
-        underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
-        },
-      },
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
-        },
-      },
-    },
-  },
+		opts = {
+			lsp_trouble = true,
+			dashboard = true,
+			neotree = true,
+			native_lsp = {
+				enabled = true,
+				underlines = {
+					errors = { "undercurl" },
+					hints = { "undercurl" },
+					warnings = { "undercurl" },
+					information = { "undercurl" },
+				},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+		},
+	},
+
+	-- Which fucking key did I map this to
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	}
 }
