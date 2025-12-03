348c7cf1229d5cf9314085a877fc2d1cb0da3e0a
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-12-02 20:28:08 -0500

    using lazyvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,32 +1,29 @@
 local o = vim.o
 
-vim.diagnostic.config({virtual_text = true}) -- Enables diagnostics to appear next to code
-vim.cmd.colorscheme "catppuccin-mocha"
-
 -- Changes working directory to the directory opened in nvim
 -- o.autochdir     = true
 
 -- tabs & indent
-o.tabstop       = 2
-o.shiftwidth    = 2
-o.expandtab     = false
-o.autoindent    = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.expandtab = false
+o.autoindent = true
 
 -- line numbers
-o.number         = true
+o.number = true
 o.relativenumber = true
 
 -- line wrapping
-o.wrap          = false
-o.linebreak			= false
+o.wrap = false
+o.linebreak = false
 
 -- appearance
 o.termguicolors = true
-o.signcolumn    = "yes"
+o.signcolumn = "yes"
 
 -- backspace
-o.backspace     = "indent,eol,start"
+o.backspace = "indent,eol,start"
 
 -- split windows
-o.splitright    = true
-o.splitbelow    = true
+o.splitright = true
+o.splitbelow = true
