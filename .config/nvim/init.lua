78dff049c12385dce187b759987ee879ab036513
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-01-15 11:37:12 -0500

    Minimizing neovim config, only using plugins that I really need and sourcing my vimrc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,2 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- vim.cmd.colorscheme "catppuccin-macchiato"
-require("mason-tool-installer").setup({
-	ensure_installed = {
-		"clangd",
-		"clang-format",
-		"codelldb",
-	},
-	auto_update = true,
-	run_on_start = true,
-})
+vim.cmd("source $HOME/.vim/vimrc")
+require("mason").setup()
