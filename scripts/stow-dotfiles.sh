db44b696980fc197bc6a593ed42b32a4d4964bdb
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-01-25 17:34:54 -0500

    diff: using var dotfiles_to_stow rather than relying on the wildcard for safety

--- scripts/stow-dotfiles.sh
+++ scripts/stow-dotfiles.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 symlink-dotfiles() {
   local path_to_modules="${1}"
-  (cd "$path_to_modules" && for mod in *; do stow -t "$HOME" "$mod"; done)
+  local dotfiles_to_stow=(bin kitty nvim vim)
+  (cd "$path_to_modules" && for mod in "${dotfiles_to_stow[@]}"; do stow -t "$HOME" "$mod"; done)
 }
 
 symlink-dotfiles "${1}"
