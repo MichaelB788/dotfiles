cb675d4f821c65b0a38fa6ac58010e7176c0cc49
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-01-27 15:05:48 -0500

    diff: added lsp keymaps

--- modules/nvim/.config/nvim/lua/lsp.lua
+++ modules/nvim/.config/nvim/lua/lsp.lua
@@ -22,15 +22,30 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('clangd', {}),
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    local bufnr = args.buf
+
+    local map = function(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+    end
+
+    map('n', '<leader>rn', vim.lsp.buf.rename, 'Rename')
+    map({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, 'Code actions')
+    map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
+    map('n', 'K', vim.lsp.buf.hover, 'Toggle hover')
+    map('n', '<leader>e', vim.diagnostic.setloclist, 'Open diagnostics')
+    map ('n', '<leader>uh', function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+    end, 'Toggle inlay hints')
+
     -- Auto-format ("lint") on save.
     -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
     if not client:supports_method('textDocument/willSaveWaitUntil')
       and client:supports_method('textDocument/formatting') then
       vim.api.nvim_create_autocmd('BufWritePre', {
         group = vim.api.nvim_create_augroup('clangd', {clear=false}),
-        buffer = args.buf,
+        buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+          vim.lsp.buf.format({ bufnr = bufnr, id = client.id, timeout_ms = 1000 })
         end,
       })
     end
