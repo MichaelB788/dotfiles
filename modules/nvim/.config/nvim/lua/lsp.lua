e2199a9f00213846829a042485d1a56d9ac90a58
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-02-01 19:22:40 -0500

    added leader prefix to lsp keybindings, removed kickstart nvim comments

--- modules/nvim/.config/nvim/lua/lsp.lua
+++ modules/nvim/.config/nvim/lua/lsp.lua
@@ -16,36 +16,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
   callback = function(event)
     local client = vim.lsp.get_client_by_id(event.data.client_id)
+    local bufnr = event.buf
 
     local map = function(keys, func, desc, mode)
       mode = mode or 'n'
       vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
     end
 
-    -- Rename the variable under your cursor.
-    --  Most Language Servers support renaming across files, etc.
-    map('rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-    -- Execute a code action, usually your cursor needs to be on top of an error
-    -- or a suggestion from your LSP for this to activate.
-    map('ca', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
-
-    -- Go to definition
-    map('gd', vim.lsp.buf.definition, '[G]oto definition')
-
-    -- WARN: This is not Goto Definition, this is Goto Declaration.
-    --  For example, in C this would take you to the header.
-    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-    -- The following code creates a keymap to toggle inlay hints in your
-    -- code, if the language server you are using supports them
-    if client and client:supports_method('textDocument/inlayHint', event.buf) then
-      map('<leader>uh', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
-    end
+    map('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
+    map('<leader>ca', vim.lsp.buf.code_action, 'goto [c]ode [a]ction', { 'n', 'x' })
+    map('<leader>gd', vim.lsp.buf.definition, '[g]oto [d]efinition')
+    map('<leader>gD', vim.lsp.buf.declaration, '[g]oto [D]eclaration')
 
     -- Auto-format ("lint") on save.
     -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-    local bufnr = event.buf
     if not client:supports_method('textDocument/willSaveWaitUntil')
       and client:supports_method('textDocument/formatting') then
       vim.api.nvim_create_autocmd('BufWritePre', {
@@ -56,41 +40,28 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end,
       })
     end
+
+    -- Keymap to toggle inlay hints
+    if client and client:supports_method('textDocument/inlayHint', event.buf) then
+      map('<leader>uh', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
+    end
   end,
 })
 
 require('blink.cmp').setup {
   keymap = {
-    -- All presets have the following mappings:
-    -- <tab>/<s-tab>: move to right/left of your snippet expansion
-    -- <c-space>: Open menu or open docs if already open
-    -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-    -- <c-e>: Hide menu
-    -- <c-k>: Toggle signature help
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
     preset = 'default',
-
-    -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-    --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
   },
-
   completion = {
     -- By default, you may press `<c-space>` to show the documentation.
     -- Optionally, set `auto_show = true` to show the documentation after a delay.
     documentation = { auto_show = false, auto_show_delay_ms = 500 },
   },
-
   sources = {
     default = { 'lsp', 'path', 'snippets' },
   },
-
   snippets = { preset = 'luasnip' },
-
-  -- See :h blink-cmp-config-fuzzy for more information
   fuzzy = { implementation = 'lua' },
-
-  -- Shows a signature help window while you type arguments for a function
   signature = { enabled = true },
 }
 
@@ -101,8 +72,7 @@ require('blink.cmp').setup {
 local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 -- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---  See `:help lsp-config` for information about keys and how to configure
+-- See `:help lsp-config` for information about keys and how to configure
 local servers = {
   clangd = {},
   rust_analyzer = {},
@@ -129,5 +99,3 @@ for name, server in pairs(servers) do
   vim.lsp.config(name, server)
   vim.lsp.enable(name)
 end
-
-require('luasnip').setup()
