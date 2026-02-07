d0ffa7d040e8270b28f2785a50cfdc0d613bb2f3
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-02-07 17:23:40 -0500

    Removed unnecessary code left over from Kickstart.nvim

--- modules/nvim/.config/nvim/lua/lsp.lua
+++ modules/nvim/.config/nvim/lua/lsp.lua
@@ -5,18 +5,14 @@ vim.pack.add({
 }, { confirm = false })
 require('mason').setup()
 
--- Ensure the servers and tools above are installed
-local ensure_installed = vim.tbl_keys(servers or {})
-vim.list_extend(ensure_installed, {
-  'lua-language-server',
-  'stylua',
-  'clangd',
-  'clang-format',
-  'rust-analyzer',
-})
-
 require('mason-tool-installer').setup {
-  ensure_installed = ensure_installed,
+  ensure_installed = {
+    'lua-language-server',
+    'stylua',
+    'clangd',
+    'clang-format',
+    'rust-analyzer',
+  },
   auto_update = true,
   run_on_start = true,
 }
@@ -24,10 +20,3 @@ require('mason-tool-installer').setup {
 -- Enable the following language servers
 -- See `:help lsp-config` for information about keys and how to configure
 vim.lsp.enable({ "clangd", "rust_analyzer", "lua_ls" })
-vim.lsp.config("clangd", {
-  capabilities = {
-    textDocument = {
-      willSaveWaitUntil = 1000
-    }
-  }
-})
