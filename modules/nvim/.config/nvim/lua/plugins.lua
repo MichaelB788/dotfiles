1a7a35abcac9b6b5653dfa7c9dee2bfb848fd831
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-02-26 21:15:49 -0500

    Added useful plugins for Markdown

--- modules/nvim/.config/nvim/lua/plugins.lua
+++ modules/nvim/.config/nvim/lua/plugins.lua
@@ -1,39 +1,41 @@
 vim.pack.add({
-  'https://github.com/nvim-mini/mini.pick',
+  "https://github.com/nvim-mini/mini.pick",
+  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/folke/tokyonight.nvim",
+  "https://github.com/folke/flash.nvim",
+  "https://github.com/numToStr/FTerm.nvim",
+  "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+  "https://github.com/iamcco/markdown-preview.nvim",
 
-  'https://github.com/stevearc/oil.nvim',
-
-  'https://github.com/folke/tokyonight.nvim',
-
-  'https://github.com/folke/flash.nvim',
-
-  'https://github.com/numToStr/FTerm.nvim',
-
-  'https://github.com/hrsh7th/nvim-cmp',
-  'https://github.com/hrsh7th/cmp-nvim-lsp',
-  'https://github.com/rafamadriz/friendly-snippets',
-  'https://github.com/garymjr/nvim-snippets',
+  "https://github.com/hrsh7th/nvim-cmp",
+  "https://github.com/hrsh7th/cmp-nvim-lsp",
+  "https://github.com/rafamadriz/friendly-snippets",
+  "https://github.com/garymjr/nvim-snippets",
 }, { confirm = false })
 
-require 'mini.pick'.setup {}
-vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
-vim.keymap.set('n', '<leader>g', ':Pick grep_live<CR>')
+require("mini.pick").setup()
+vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
+vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
+vim.keymap.set("n", "<leader>g", ":Pick grep_live<CR>")
 
-require 'oil'.setup {}
-vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+require("oil").setup()
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
 
-require "tokyonight".setup {}
-vim.cmd.colorscheme "tokyonight-storm"
+require("tokyonight").setup()
+vim.cmd.colorscheme("tokyonight-storm")
 
-require 'flash'.setup {}
-vim.keymap.set('n', '<leader>j', '<cmd>lua require"flash".jump()<cr>')
+require("flash").setup()
+vim.keymap.set("n", "<leader>j", '<cmd>lua require"flash".jump()<cr>')
 
-require 'FTerm'.setup {}
-vim.keymap.set('n', '<C-t>', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<C-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+require("FTerm").setup()
+vim.keymap.set("n", "<C-t>", '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set("t", "<C-t>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 
-local cmp = require 'cmp'
+require("render-markdown").setup()
+
+vim.fn["mkdp#util#install"]()
+
+local cmp = require("cmp")
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
@@ -45,21 +47,18 @@ cmp.setup({
     max_view_entries = 10,
   },
   formatting = {
-    fields = { 'abbr' },
+    fields = { "abbr" },
   },
   window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
-  sources = cmp.config.sources(
-    { { name = 'nvim_lsp' }, },
-    { { name = 'buffer' }, }
-  )
+  sources = cmp.config.sources({ { name = "nvim_lsp" } }, { { name = "buffer" } }),
 })
