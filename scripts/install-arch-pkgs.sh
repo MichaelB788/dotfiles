2d3467df95f8f9a3726ed213b0da13675e6ea0e3
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-03-03 17:56:31 -0500

    Seperated wayland and xorg packages

--- scripts/install-arch-pkgs.sh
+++ scripts/install-arch-pkgs.sh
@@ -8,14 +8,23 @@ if ! command -v yay &>/dev/null; then
   (cd yay && makepkg -si)
 fi
 
+WAYLAND_PKGS=(
+  sway swaybg swayidle swaylock waybar xdg-desktop-portal-wlr wofi wl-clipboard
+)
+
+XORG_PKGS=(
+  i3-wm i3status i3lock xorg-server xorg-xinit xterm
+)
+
 YAY_PACKAGES=(
   neovim-nightly-bin jetbrains-toolbox
 )
 
 PACMAN_PKGS=(
-  sway swaybg swayidle swaylock waybar xdg-desktop-portal-wlr wofi wl-clipboard
-  vim zed fzf kitty lazygit stow clang cmake npm rust-analyzer
+  vim zed fzf kitty lazygit stow clang cmake npm rust-analyzer thunar picom rofi
+  network-manager-applet
 )
 
+sudo pacman -S --needed --noconfirm "${XORG_PKGS[@]}"
 sudo pacman -S --needed --noconfirm "${PACMAN_PKGS[@]}"
 yay -S --needed --noconfirm "${YAY_PACKAGES[@]}"
