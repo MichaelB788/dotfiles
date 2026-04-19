5c428eef458df2f309d4d88467128ebd6480e3aa
Mike B <michaelbarrientos788@gmail.com>
2026-04-19 15:29:54 -0400

    Updated script to be Void compatible

--- scripts/install-pkgs.sh
+++ scripts/install-pkgs.sh
@@ -5,5 +5,10 @@ set -euo pipefail
 SCRIPT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)
 
 # Install packages
-sudo pacman -Syu
-xargs -a "${SCRIPT_PATH}/arch-deps.txt" sudo pacman -S --needed
+if command -v pacman >/dev/null; then
+  sudo pacman -Syu
+  xargs -a "${SCRIPT_PATH}/arch-deps.txt" sudo pacman -S --needed
+elif command -v xbps-install >/dev/null; then
+  sudo xbps-install -Su
+  xargs -a "${SCRIPT_PATH}/void-deps.txt" sudo xbps-install
+fi
