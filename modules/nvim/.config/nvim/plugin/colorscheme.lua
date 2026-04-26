b361c303bd428727b251d49bf1ae51d136a02513
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-04-26 15:27:55 -0400

    Tokyo Night theme

--- modules/nvim/.config/nvim/plugin/colorscheme.lua
+++ modules/nvim/.config/nvim/plugin/colorscheme.lua
@@ -1,12 +1,7 @@
-vim.pack.add({ "https://github.com/edeneast/nightfox.nvim" }, { confirm = false })
+vim.pack.add({ "https://github.com/folke/tokyonight.nvim" }, { confirm = false })
 
--- Default options
-require('nightfox').setup({
-  options = {
-    transparent = true,     -- Disable setting background
-    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false,   -- Non focused panes set to alternative background
-  },
+require("tokyonight").setup({
+  transparent = true
 })
 
-vim.cmd [[colorscheme duskfox]]
+vim.cmd[[colorscheme tokyonight]]
