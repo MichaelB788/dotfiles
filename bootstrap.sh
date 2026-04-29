8291dd970a500f6bdc712180dd51b87b6b61b455
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-04-29 16:29:25 -0400

    Added Arch and Debian split dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,31 +3,29 @@
 set -euo pipefail
 
 DOTFILES_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)
+PKGS_PATH="$DOTFILES_PATH/packages"
 
 # Stows the module should the binary exist
 stow_mod() {
-  if command -v "$1" >/dev/null; then
-    stow -d "$DOTFILES_PATH/modules" -t "$HOME" "$1"
-  fi
+  stow -d "$DOTFILES_PATH/modules" -t "$HOME" "$1"
 }
 
 # Upgrade, then install packages
-sudo apt full-upgrade
-xargs -a "${DOTFILES_PATH}/apt_pkgs.txt" sudo apt install 
-
-# Install neovim if it doesn't exist
-if ! command -v nvim >/dev/null; then
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-  sudo rm -rf /opt/nvim-linux-x86_64
-  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-
-  echo "export PATH='$PATH:/opt/nvim-linux-x86_64/bin'" >> $HOME/.bashrc
+if command -v apt >/dev/null; then
+  sudo apt full-upgrade
+  xargs -a "${PKGS_PATH}/apt.txt" sudo apt install
+elif command -v pacman >/dev/null; then
+  sudo pacman -Syu
+  xargs -a "${PKGS_PATH}/pacman.txt" sudo pacman -S
 fi
 
 # Stow modules
 stow_mod "vim"
 stow_mod "nvim"
 stow_mod "kitty"
-stow_mod "zed"
-stow_mod "i3"
-stow_mod "sway"
+
+read -rp "Stow TWM config [y/N]? " -n 1
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  stow_mod "i3"
+  stow_mod "sway"
+fi
