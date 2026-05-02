fc339089330a488da1708122c94093479f51844e
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-05-02 22:39:28 +0000

    Arch and OpenSUSE combatibility

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,26 +4,13 @@ set -euo pipefail
 
 DOTFILES_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)
 
-# Stows the module should the binary exist
-stow_mod() {
-  stow -d "$DOTFILES_PATH/modules" -t "$HOME" "$1"
-}
-
-# Upgrade, then install packages
-if command -v zypper >/dev/null; then
+PKGS_PATH="$DOTFILES_PATH/pkgs"
+if command -v pacman >/dev/null; then
+  sudo pacman -Syu
+  xargs -a "$PKGS_PATH/pacman.txt" sudo pacman -S --needed
+elif command -v zypper >/dev/null; then
   sudo zypper dup
-  xargs -a "$DOTFILES_PATH/zypper.txt" sudo zypper install
+  xargs -a "$PKGS_PATH/zypper.txt" sudo zypper install
 fi
 
-# Stow modules
-stow_mod "vim"
-stow_mod "nvim"
-stow_mod "kitty"
-
-read -rp "Would you like to setup sway? [y/N] " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sudo zypper addlock sway-branding-openSUSE
-  sudo zypper install sway wofi
-  stow_mod "sway"
-fi
+stow -d "$DOTFILES_PATH/modules" -t "$HOME" vim nvim kitty sway zed i3
