ca8d96493ae7e17284575845b1ba0b0e7f01f527
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-05-04 00:36:31 -0400

    Fixed filename, added more packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ elif command -v zypper >/dev/null; then
   xargs -a "$PKGS_PATH/zypper.txt" sudo zypper install
 elif command -v apt >/dev/null; then
   sudo apt update && sudo apt upgrade
-  xargs -a "$PKGS_PATH/pacman.txt" sudo apt install
+  xargs -a "$PKGS_PATH/apt.txt" sudo apt install
 fi
 
 stow -d "$DOTFILES_PATH/modules" -t "$HOME" vim nvim kitty sway zed i3
