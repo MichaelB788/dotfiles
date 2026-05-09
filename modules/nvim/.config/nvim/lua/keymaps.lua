fb40864c0e993b2935dae673747e63ebc7cbc80d
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-05-09 17:25:31 -0400

    Removing some keybindings to better learn default vim motions

--- modules/nvim/.config/nvim/lua/keymaps.lua
+++ modules/nvim/.config/nvim/lua/keymaps.lua
@@ -1,22 +1,11 @@
--- Write to all files
-vim.keymap.set("n", "<C-s>", ":wa<CR>")
-
 -- Tabs and splits
 vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
-vim.keymap.set("n", "<leader>sv", ":vsp<CR>")
-vim.keymap.set("n", "<leader>sh", ":sp<CR>")
 
 -- Resize editors
 vim.keymap.set("n", "=", ":vertical resize +5<CR>")
 vim.keymap.set("n", "-", ":vertical resize -5<CR>")
 vim.keymap.set("n", "+", ":horizontal resize +2<CR>")
 vim.keymap.set("n", "_", ":horizontal resize -2<CR>")
-
--- Editor navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Tab navigation
 vim.keymap.set("n", "<S-l>", "gt")
