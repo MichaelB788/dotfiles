3e6a04e3b19a023e6672055f8248846791650b32
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-05-28 22:43:25 -0400

    Stows all files inside modules/ without the need to hard code it

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 DOTFILES_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)
 PKGS_PATH="$DOTFILES_PATH/pkgs"
+MODULES_PATH="$DOTFILES_PATH/modules"
 
-# Install packages
 if command -v pacman >/dev/null; then # Arch setup
   sudo pacman -Syu --noconfirm
   xargs -a "$PKGS_PATH/pacman.txt" sudo pacman -S --needed --noconfirm
@@ -13,9 +13,10 @@ elif command -v xbps-install >/dev/null; then # Void setup
   sudo xbps-install -Syu
   xargs -a "$PKGS_PATH/xbps.txt" sudo xbps-install -y
 elif command -v apt >/dev/null; then # Debian setup
-  sudo apt update && sudo apt upgrade
+  sudo apt update
+  sudo apt upgrade
   xargs -a "$PKGS_PATH/apt.txt" sudo apt install
 fi
 
 # Stow dotfiles
-stow -d "$DOTFILES_PATH/modules" -t "$HOME" vim nvim kitty sway zed i3
+(cd "$MODULES_PATH" && stow -Rt "$HOME" */)
