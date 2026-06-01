6e3556c74592170e8d6586c38fc490a9bca73fae
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-05-31 20:15:33 -0400

    Added Fedora
    Clang tools extra

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,11 @@ MODULES_PATH="$DOTFILES_PATH/modules"
 if command -v pacman >/dev/null; then # Arch setup
   sudo pacman -Syu --noconfirm
   xargs -a "$PKGS_PATH/pacman.txt" sudo pacman -S --needed --noconfirm
-elif command -v xbps-install >/dev/null; then # Void setup
-  sudo xbps-install -Syu
-  xargs -a "$PKGS_PATH/xbps.txt" sudo xbps-install -y
+elif command -v dnf >/dev/null; then # Fedora setup
+  sudo dnf -y update
+  sudo dnf copr enable -y atim/lazygit
+  sudo dnf copr enable -y scottames/ghostty
+  xargs -a "$PKGS_PATH/dnf.txt" sudo dnf install -y
 elif command -v apt >/dev/null; then # Debian setup
   sudo apt update
   sudo apt upgrade
