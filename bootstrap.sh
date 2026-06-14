62b123e3ef3d139d0d8b8e10fe7179ce2d75e8e6
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-06-14 10:27:23 -0400

    Fedora and Void assumptions

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,19 +6,12 @@ DOTFILES_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)
 PKGS_PATH="$DOTFILES_PATH/pkgs"
 MODULES_PATH="$DOTFILES_PATH/modules"
 
-if command -v pacman >/dev/null; then # Arch setup
-  sudo pacman -Syu --noconfirm
-  xargs -a "$PKGS_PATH/pacman.txt" sudo pacman -S --needed --noconfirm
-elif command -v dnf >/dev/null; then # Fedora setup
+if command -v dnf >/dev/null; then # Fedora setup
   sudo dnf -y update
   xargs -a "$PKGS_PATH/dnf.txt" sudo dnf install -y
-elif command -v xbps-install >/dev/null; then # Void setup
+elif command -v xbps-install >/dev/null; then # Void Setup
   sudo xbps-install -Syu
   xargs -a "$PKGS_PATH/xbps.txt" sudo xbps-install -y
-elif command -v apt >/dev/null; then # Debian setup
-  sudo apt update
-  sudo apt upgrade
-  xargs -a "$PKGS_PATH/apt.txt" sudo apt install
 fi
 
 # Stow dotfiles
