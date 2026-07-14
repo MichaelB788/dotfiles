226498945c60f35acef0664821180ccf4656e0cc
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-14 14:18:44 -0400

    Updated keymaps and options

--- modules/nvim/.config/nvim/lua/keymaps.lua
+++ modules/nvim/.config/nvim/lua/keymaps.lua
@@ -1,33 +1,54 @@
--- Tabs and splits
-vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
 
--- Resize editors
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic Config & Keymaps
+--  See `:help vim.diagnostic.Opts`
+vim.diagnostic.config {
+  update_in_insert = false,
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = { min = vim.diagnostic.severity.WARN } },
+
+  -- Can switch between these as you prefer
+  virtual_text = true,   -- Text shows up at the end of the line
+  virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+
+  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+  jump = {
+    on_jump = function(_, bufnr)
+      vim.diagnostic.open_float {
+        bufnr = bufnr,
+        scope = 'cursor',
+        focus = false,
+      }
+    end,
+  },
+}
+
+vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { desc = 'Open [D]iagnostic quickfix list' })
+
+-- Tabs
+vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
+vim.keymap.set("n", "<S-l>", "gt")
+vim.keymap.set("n", "<S-h>", "gT")
+
+-- Resizing
 vim.keymap.set("n", "=", ":vertical resize +5<CR>")
 vim.keymap.set("n", "-", ":vertical resize -5<CR>")
 vim.keymap.set("n", "+", ":horizontal resize +2<CR>")
 vim.keymap.set("n", "_", ":horizontal resize -2<CR>")
 
--- Tab navigation
-vim.keymap.set("n", "<S-l>", "gt")
-vim.keymap.set("n", "<S-h>", "gT")
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
 
--- Terminal keymaps
-local term_buf = nil
-
-vim.keymap.set("n", "<C-t>", function()
-  if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
-    vim.cmd [[tabnew]]
-    vim.api.nvim_win_set_buf(0, term_buf)
-    vim.cmd [[startinsert]]
-  else
-    vim.cmd [[tabnew | terminal]]
-    term_buf = vim.api.nvim_get_current_buf()
-    vim.cmd [[startinsert]]
-  end
-end)
-
-vim.keymap.set("t", "<C-t>", [[<C-\><C-n>:hide<CR>]])
-vim.keymap.set("t", "<C-x>", [[<C-\><C-n>]])
-
--- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  callback = function() vim.hl.on_yank() end,
+})
