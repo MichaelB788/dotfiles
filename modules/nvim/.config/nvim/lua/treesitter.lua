3234c9134b16c9aa92b7a9dff12211db5af25f31
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-07-22 20:23:08 -0400

    Removing treesitter for neovim

--- modules/nvim/.config/nvim/lua/treesitter.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'lua', 'markdown', 'vim', 'vimdoc', 'python', 'rust', 'c', 'cpp' },
-  callback = function()
-    vim.treesitter.start()
-  end,
-})
